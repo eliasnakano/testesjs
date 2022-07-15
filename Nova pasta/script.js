9efa9be425c0d10abf06b7d9c87fcfc4191969d1
eliasnakano <https://github.com/eliasnakano/OnePage.git>
2022-07-14 23:41:23 -0300

    primeirocommit

---
+++ Nova pasta/script.js
@@ -0,0 +1,27 @@
+
+
+var n1 = prompt('Digite um numero');
+n1 = parseInt (n1);
+
+var n2 = prompt('Digite outro numero');
+n2 = parseInt (n2);
+
+var media = n1 + n2 / 2;
+
+if (media >= 6 ){
+
+    alert('Sua Media e ' + media + ' Parabéns');
+}
+
+else{
+     alert('Sua media foi ' + media + ' Infelizmente foi reprovado');
+}
+
+
+console.log(media);
+
+
+
+
+
+
